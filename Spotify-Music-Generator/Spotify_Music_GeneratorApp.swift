ca4f45f578b5c801794fd5d856fe68e0fc600f0c
gpw03 <gwilli17@uccs.edu>
2024-07-17 17:19:05 -0600

    Initial Commit

---
+++ Spotify-Music-Generator/Spotify_Music_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_Music_GeneratorApp.swift
+//  Spotify-Music-Generator
+//
+//  Created by Gabriel Williams on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_Music_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

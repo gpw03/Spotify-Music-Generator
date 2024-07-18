91a8909d50da546c3bb2ffd0f21e4510f9e0af92
gpw03 <gwilli17@uccs.edu>
2024-07-18 10:07:03 -0600

    Added the spotify SDK package

--- Spotify-Music-Generator/ContentView.swift
+++ Spotify-Music-Generator/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack
+        {            Circle()
+                .fill(.blue)
+                .padding()
+                .overlay(
+                    Image(systemName: "figure.archery")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                )
+            Text("Archery!")
+                .font(.title)
         }
-        .padding()
     }
 }
 
